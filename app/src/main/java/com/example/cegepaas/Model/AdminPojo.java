0b897ef3b1ff385a97ed1d7065d69f5e8f5c9cd6
Ambarish Kesavaram <ambarish.kesavaram@gmail.com>
2020-09-30 10:17:42 -0400

    Added AdminPojo model class for admin login

---
+++ app/src/main/java/com/example/cegepaas/Model/AdminPojo.java
@@ -0,0 +1,32 @@
+package com.example.cegepaas.Model;
+
+public class AdminPojo {
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public AdminPojo(String uname, String pwd) {
+        this.uname = uname;
+        this.pwd = pwd;
+    }
+
+    private String uname;
+    private String pwd;
+
+    public AdminPojo()
+    {
+
+    }
+}

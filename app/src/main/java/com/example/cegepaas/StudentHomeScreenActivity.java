06b74e37341382603b51a9e67e77258522365398
Gagandeep Singh Saini <gagan_singh18@protonmail.com>
2020-09-30 18:37:53 -0400

    Get advisors data from firebase

--- app/src/main/java/com/example/cegepaas/StudentHomeScreenActivity.java
+++ app/src/main/java/com/example/cegepaas/StudentHomeScreenActivity.java
@@ -1,14 +1,87 @@
 package com.example.cegepaas;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.cegepaas.Adapters.StudentHomeAdapter;
+import com.example.cegepaas.Model.AdvisorsPojo;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class StudentHomeScreenActivity extends AppCompatActivity {
 
+    private ActionBarDrawerToggle t;
+    private NavigationView nv;
+    private DrawerLayout dl;
+    ListView lv;
+    ProgressDialog progressDialog;
+    private List<AdvisorsPojo> mAdvisors;
+    DatabaseReference dbAdvisors;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_home_screen);
+
+        ActionBar mActionBar=getSupportActionBar();
+        mActionBar.setDisplayShowHomeEnabled(false);
+        mActionBar.setDisplayShowTitleEnabled(false);
+        mActionBar.setDisplayShowCustomEnabled(true);
+
+        getSupportActionBar().setTitle("Home");
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        lv=(ListView)findViewById(R.id.lv);
+
+        getAdvisorsDetails();
     }
+    private void getAdvisorsDetails(){
+        mAdvisors = new ArrayList<>();
+        progressDialog=new ProgressDialog(StudentHomeScreenActivity.this);
+        progressDialog.setTitle("Please Wait data is being Loaded");
+        progressDialog.show();
+        dbAdvisors = FirebaseDatabase.getInstance().getReference("Advisor_Details");
+        dbAdvisors.addListenerForSingleValueEvent(valueEventListener);
+    }
+    ValueEventListener valueEventListener = new ValueEventListener() {
+        @Override
+        public void onDataChange(DataSnapshot dataSnapshot) {
+            progressDialog.dismiss();
+            mAdvisors.clear();
+            if (dataSnapshot.exists()) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    AdvisorsPojo artist = snapshot.getValue(AdvisorsPojo.class);
+                    mAdvisors.add(artist);
+                }
+                if(mAdvisors.size()>0){
+                    lv.setAdapter(new StudentHomeAdapter(mAdvisors, StudentHomeScreenActivity.this));
+                }
+            }
+            else {
+                Toast.makeText(StudentHomeScreenActivity.this, "No data Found", Toast.LENGTH_SHORT).show();
+            }
+        }
+        @Override
+        public void onCancelled(DatabaseError databaseError) {
+            progressDialog.dismiss();
+
+        }
+    };
+
 }

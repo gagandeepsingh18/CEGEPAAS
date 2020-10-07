63117c0c6dc44b6c99040894013bae132c19f780
Harwinder <happyhayer20@gmail.com>
2020-10-07 14:32:26 -0700

    AdvisorMeetingAdapter java class completed..

--- app/src/main/java/com/example/cegepaas/Adapters/AdvisorMeetingAdapter.java
+++ app/src/main/java/com/example/cegepaas/Adapters/AdvisorMeetingAdapter.java
@@ -1,4 +1,103 @@
 package com.example.cegepaas.Adapters;
 
-public class AdvisorMeetingAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import com.example.cegepaas.R;
+import com.example.cegepaas.Model.AdvisorBookingPojo;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class AdvisorMeetingAdapter extends BaseAdapter {
+    List<AdvisorBookingPojo> ar;
+    Context cnt;
+    public AdvisorMeetingAdapter(List<AdvisorBookingPojo> ar, Context cnt) {
+        this.ar = ar;
+        this.cnt = cnt;
+    }
+    @Override
+    public int getCount() {
+        return ar.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return i;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(final int pos, View view, ViewGroup viewGroup) {
+        LayoutInflater obj1 = (LayoutInflater) cnt.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View obj2 = obj1.inflate(R.layout.adapter_student_meeting, null);
+
+        TextView tv_name = (TextView) obj2.findViewById(R.id.tv_name);
+        tv_name.setText("Student Uname  :"+ar.get(pos).getBooked_by());
+
+        TextView tv_date_time = (TextView) obj2.findViewById(R.id.tv_date_time);
+        tv_date_time.setText("Date & Time :"+ar.get(pos).getBooked_date()+"  "+ar.get(pos).getBooked_time());
+
+        TextView tv_desc = (TextView) obj2.findViewById(R.id.tv_desc);
+        tv_desc.setText("Desc  :"+ar.get(pos).getDescription());
+        return obj2;
+    }
+
+    private void UpdateStatus(final String status,final String time_stamp) {
+        final DatabaseReference RootRef;
+        RootRef = FirebaseDatabase.getInstance().getReference();
+
+        RootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                if ((dataSnapshot.child("Advisor_Booking").child(time_stamp).exists()))
+                {
+                    HashMap<String, Object> userdataMap = new HashMap<>();
+                    /*userdataMap.put("image", adv.getImage());
+                    userdataMap.put("name", adv.getName());
+                    userdataMap.put("email", adv.getEmail());
+                    userdataMap.put("username", adv.getUsername());
+                    userdataMap.put("password", adv.getPassword());*/
+                    userdataMap.put("status", status);
+                    RootRef.child("Advisor_Booking").child(time_stamp).updateChildren(userdataMap)
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task)
+                                {
+                                    if (task.isSuccessful())
+                                    {
+                                        Toast.makeText(cnt, "Status is updated successfully.", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
+
+            }
+
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+    }
 }

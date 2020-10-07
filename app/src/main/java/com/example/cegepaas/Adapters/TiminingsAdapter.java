f35db160b3f71307d6556794a4dd3e4ef6bcc123
Puneeth996 <52050064+Puneeth996@users.noreply.github.com>
2020-10-07 01:29:59 -0400

    Timingsadapter full

--- app/src/main/java/com/example/cegepaas/Adapters/TiminingsAdapter.java
+++ app/src/main/java/com/example/cegepaas/Adapters/TiminingsAdapter.java
@@ -36,3 +36,24 @@ public class TiminingsAdapter extends BaseAdapter {
     public long getItemId(int i) {
         return i;
     }
+
+    @Override
+    public View getView(final int pos, View view, ViewGroup viewGroup) {
+        LayoutInflater obj1 = (LayoutInflater) cnt.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View obj2 = obj1.inflate(R.layout.adapter_timings, null);
+
+
+        TextView tv_schedule_time = (TextView) obj2.findViewById(R.id.tv_schedule_time);
+        tv_schedule_time.setText(ar.get(pos).getTime());
+
+        tv_schedule_time.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(cnt, "Succussfully Allocated "+ar.get(pos).getTime()+"  Slot", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        return obj2;
+    }
+}

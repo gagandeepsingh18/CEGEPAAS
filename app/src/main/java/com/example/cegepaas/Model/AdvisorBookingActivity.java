d0efb502d6848078b1ba835802582b2718c4aaca
prasanthreddythumma <prasanthreddy770@gmail.com>
2020-10-06 23:51:45 -0700

    AdvisorBookingActivity front End part

---
+++ app/src/main/java/com/example/cegepaas/Model/AdvisorBookingActivity.java
@@ -0,0 +1,18 @@
+package com.example.cegepaas.Model;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cegepaas.R;
+
+public class AdvisorBookingActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_advisor_booking);
+    }
+}

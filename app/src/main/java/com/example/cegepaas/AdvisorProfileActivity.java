8c9c1c3bb2f7270a9ecd7f6116f92b0ff7d7cf1c
Gagandeep Singh Saini <gagan_singh18@protonmail.com>
2020-10-13 18:13:36 -0400

    Getting user details

--- app/src/main/java/com/example/cegepaas/AdvisorProfileActivity.java
+++ app/src/main/java/com/example/cegepaas/AdvisorProfileActivity.java
@@ -1,19 +1,58 @@
 package com.example.cegepaas;
 
-
-import android.os.Bundle;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class AdvisorProfileActivity extends AppCompatActivity {
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.example.cegepaas.Model.AdvisorsPojo;
+import com.example.cegepaas.Model.Users;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class AdvisorProfileActivity extends AppCompatActivity {
+    ImageView profile;
+    TextView name,email,id;
+    private String parentDbName = "Advisor_Details";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_advisor_profile);
 
+        getSupportActionBar().setTitle("Profile");
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        profile = findViewById(R.id.advisor_image);
+        name = findViewById(R.id.advisor_name);
+        email = findViewById(R.id.advisorEmail);
+        id = findViewById(R.id.advisorId);
+        SharedPreferences sp=getSharedPreferences("AA",0);
+        getAdvisorDetails(sp.getString("auname","-"));
+    }
+
+    private void getAdvisorDetails(String auname) {
     }
 
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
 }

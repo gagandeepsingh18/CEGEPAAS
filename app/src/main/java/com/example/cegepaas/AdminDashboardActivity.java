b8f6640aab3d9676db38ebbc12590a110b76243f
prasanthreddythumma <prasanthreddy770@gmail.com>
2020-10-14 13:10:20 -0700

    Changes in the packages

--- app/src/main/java/com/example/cegepaas/AdminDashboardActivity.java
+++ app/src/main/java/com/example/cegepaas/AdminDashboardActivity.java
@@ -14,7 +14,6 @@ import android.view.MenuItem;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import com.example.cegepaas.Adapters.AdminAdvisorDeleteAdapter;
 import com.example.cegepaas.Adapters.AdminHomeAdapter;
 import com.example.cegepaas.Model.AdvisorsPojo;
 import com.google.android.material.navigation.NavigationView;
@@ -150,78 +149,4 @@ public class AdminDashboardActivity extends AppCompatActivity {
 
         }
     };
-
-    public static class AdminAdvisorDeleteActivity extends AppCompatActivity {
-        private ActionBarDrawerToggle t;
-        private NavigationView nv;
-        private DrawerLayout dl;
-        ListView lv;
-
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_admin_advisor_delete);
-            ActionBar mActionBar = getSupportActionBar();
-            mActionBar.setDisplayShowHomeEnabled(false);
-            mActionBar.setDisplayShowTitleEnabled(false);
-            mActionBar.setDisplayShowCustomEnabled(true);
-
-            getSupportActionBar().setTitle("Advisor List");
-            getSupportActionBar().setHomeButtonEnabled(true);
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-            lv = (ListView) findViewById(R.id.lv);
-
-            getAdvisorsDetails();
-        }
-
-        @Override
-        public boolean onOptionsItemSelected(MenuItem item) {
-            switch (item.getItemId()) {
-                case android.R.id.home:
-                    this.finish();
-                    return true;
-                default:
-                    return super.onOptionsItemSelected(item);
-            }
-        }
-
-        ProgressDialog progressDialog;
-        private List<AdvisorsPojo> mAdvisors;
-        DatabaseReference dbAdvisors;
-
-        private void getAdvisorsDetails() {
-            mAdvisors = new ArrayList<>();
-            progressDialog = new ProgressDialog(AdminAdvisorDeleteActivity.this);
-            progressDialog.setTitle("Please Wait data is being Loaded");
-            progressDialog.show();
-            dbAdvisors = FirebaseDatabase.getInstance().getReference("Advisor_Details");
-            dbAdvisors.addListenerForSingleValueEvent(valueEventListener);
-        }
-
-        ValueEventListener valueEventListener = new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                progressDialog.dismiss();
-                mAdvisors.clear();
-                if (dataSnapshot.exists()) {
-                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                        AdvisorsPojo artist = snapshot.getValue(AdvisorsPojo.class);
-                        mAdvisors.add(artist);
-                    }
-                    if (mAdvisors.size() > 0) {
-                        lv.setAdapter(new AdminAdvisorDeleteAdapter(mAdvisors, AdminAdvisorDeleteActivity.this));
-                    }
-                } else {
-                    Toast.makeText(AdminAdvisorDeleteActivity.this, "No data Found", Toast.LENGTH_SHORT).show();
-                }
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-                progressDialog.dismiss();
-
-            }
-        };
-    }
 }

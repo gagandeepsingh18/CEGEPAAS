ea0b1f01e266ee26137323c23355effdf468fd8b
prasanthreddythumma <prasanthreddy770@gmail.com>
2020-10-20 19:25:35 -0700

    AdvisorEditProfileActivity

---
+++ app/src/main/java/com/example/cegepaas/AdvisorEditProfile.java
@@ -0,0 +1,14 @@
+package com.example.cegepaas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdvisorEditProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_advisor_edit_profile);
+    }
+}

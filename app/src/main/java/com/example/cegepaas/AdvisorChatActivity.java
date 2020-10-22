3638132f7c117abbdc3ca0bf638cfde4c761c8ac
Manjinder Singh <manjinders2212@gmail.com>
2020-10-22 09:35:15 -0400

    AdvisorChatActivity.java class.code updating

--- app/src/main/java/com/example/cegepaas/AdvisorChatActivity.java
+++ app/src/main/java/com/example/cegepaas/AdvisorChatActivity.java
@@ -22,39 +22,7 @@ public class AdvisorChatActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     DatabaseReference reference;
 
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chat);
-        text_message = findViewById(R.id.text_send);
-        studentId = getIntent().getStringExtra("sname");
-        text_message = findViewById(R.id.text_send);
-        sendMessage = findViewById(R.id.btn_send);
-        recyclerView = findViewById(R.id.recycler_view);
-
-        recyclerView.setHasFixedSize(true);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
-        linearLayoutManager.setStackFromEnd(true);
-        recyclerView.setLayoutManager(linearLayoutManager);
-
-        SharedPreferences sp = getSharedPreferences("AA",0);
-        advisorId = sp.getString("auname","-");
-
-        text_message.setText(advisorId);
-        sendMessage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                message = text_message.getText().toString();
-                if(!message.equals("")){
-                    sendMessage(advisorId,studentId,message);
-                }else {
-                    Toast.makeText(AdvisorChatActivity.this, "Can't send empty message..", Toast.LENGTH_SHORT).show();
-                }
-                text_message.setText("");
-            }
-        });
-        readMessages(advisorId,studentId,message);
-    }
+   
 
     private void sendMessage(String sender, String receiver, String message) {
         reference = FirebaseDatabase.getInstance().getReference();
@@ -65,7 +33,27 @@ public class AdvisorChatActivity extends AppCompatActivity {
         reference.child("Chats").child(""+System.currentTimeMillis()/1000).setValue(hashMap);
     }
 
-   
+    private void readMessages(String sender, String receiver, String message){
+        chatList = new ArrayList<>();
+        reference = FirebaseDatabase.getInstance().getReference("Chats");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                chatList.clear();
+                for(DataSnapshot snapshot:dataSnapshot.getChildren()){
+                    ChatPojo chat = snapshot.getValue(ChatPojo.class);
+                    if((chat.getReceiver().equals(receiver) && chat.getSender().equals(sender)) ||
+                            (chat.getReceiver().equals(sender) && chat.getSender().equals(receiver))){
+                        chatList.add(chat);
+                    }
+                    recyclerView.setAdapter(new ChatAdapter(chatList,AdvisorChatActivity.this,advisorId));
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
         });
 
     }

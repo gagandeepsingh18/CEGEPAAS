e68847337b32a4f4391cce16b19b567b89700703
Puneeth996 <52050064+Puneeth996@users.noreply.github.com>
2020-10-24 00:38:37 -0400

    AdvisorsPojo changes

--- app/src/main/java/com/example/cegepaas/Model/AdvisorsPojo.java
+++ app/src/main/java/com/example/cegepaas/Model/AdvisorsPojo.java
@@ -7,18 +7,39 @@ public class AdvisorsPojo {
     private String image;
     private String password;
     private String status;
+    private String phoneNumber;
+    private String campus;
+    private String department;
+    private String workingDays;
+    private String description;
 
     public AdvisorsPojo() {
 
     }
 
-    public AdvisorsPojo(String username, String email, String name, String image, String password, String status) {
+    public AdvisorsPojo(String username, String email, String name, String image, String phoneNumber, String campus, String department, String workingDays, String description) {
+        this.username = username;
+        this.email = email;
+        this.name = name;
+        this.image = image;
+        this.phoneNumber = phoneNumber;
+        this.campus = campus;
+        this.department = department;
+        this.workingDays = workingDays;
+        this.description = description;
+    }
+    public AdvisorsPojo(String username, String email, String name, String image, String password, String status, String phoneNumber, String campus, String department, String workingDays, String description) {
         this.username = username;
         this.email = email;
         this.name = name;
         this.image = image;
         this.password = password;
         this.status = status;
+        this.phoneNumber = phoneNumber;
+        this.campus = campus;
+        this.department = department;
+        this.workingDays = workingDays;
+        this.description = description;
     }
 
     public AdvisorsPojo(String username, String email, String name, String password, String status) {
@@ -27,6 +48,45 @@ public class AdvisorsPojo {
         this.name = name;
         this.password = password;
         this.status = status;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getCampus() {
+        return campus;
+    }
+
+    public void setCampus(String campus) {
+        this.campus = campus;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getWorkingDays() {
+        return workingDays;
+    }
+
+    public void setWorkingDays(String workingDays) {
+        this.workingDays = workingDays;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public String getImage() {

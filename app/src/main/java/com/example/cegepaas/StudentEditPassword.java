2c4de1457f6cee4333ec19193211a628a60eaf5f
Puneeth996 <52050064+Puneeth996@users.noreply.github.com>
2020-11-02 12:01:49 -0500

    StudentEditPassword changes

--- app/src/main/java/com/example/cegepaas/StudentEditPassword.java
+++ app/src/main/java/com/example/cegepaas/StudentEditPassword.java
@@ -13,6 +13,8 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.cegepaas.Model.AdvisorsPojo;
+import com.example.cegepaas.Model.Users;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
@@ -28,6 +30,7 @@ public class StudentEditPassword extends AppCompatActivity {
     TextView student_name, current_pass;
     Button cancel, update_password;
     DatabaseReference dbStudent;
+    String studentId;
     private String parentDbName = "Student_Details";
 
     @Override
@@ -37,7 +40,8 @@ public class StudentEditPassword extends AppCompatActivity {
         getSupportActionBar().setTitle("Change Password");
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        Intent data = getIntent();
+
+        studentId = getSharedPreferences("AA",0).getString("suname","-");
         current_pass = findViewById(R.id.currentPass);
         student_name = findViewById(R.id.student_name1);
         password1 = findViewById(R.id.new_password);
@@ -45,8 +49,7 @@ public class StudentEditPassword extends AppCompatActivity {
         cancel = findViewById(R.id.p_cancel);
         update_password = findViewById(R.id.student_password_edit);
 
-        current_pass.setText(data.getStringExtra("password"));
-        student_name.setText(data.getStringExtra("name"));
+        getCurrentPassword(studentId);
         cancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -58,54 +61,72 @@ public class StudentEditPassword extends AppCompatActivity {
         update_password.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SharedPreferences sp = getSharedPreferences("AA", 0);
-                String studentId = sp.getString("suname", "-");
-                dbStudent = FirebaseDatabase.getInstance().getReference();
                 String pass1 = password1.getText().toString();
                 String pass2 = password2.getText().toString();
-                if (pass1.equals(pass2)) {
-                    dbStudent.addListenerForSingleValueEvent(new ValueEventListener() {
-                        @Override
-                        public void onDataChange(@NonNull DataSnapshot snapshot) {
-                            HashMap<String, Object> passwordDetails = new HashMap<>();
-                            passwordDetails.put("password", pass1);
-
-
-                            dbStudent.child(parentDbName).child(studentId).updateChildren(passwordDetails)
-                                    .addOnCompleteListener(new OnCompleteListener() {
-                                        @Override
-                                        public void onComplete(@NonNull Task task) {
-                                            if (task.isSuccessful()) {
-                                                Toast.makeText(StudentEditPassword.this, "Successfully updated", Toast.LENGTH_SHORT).show();
-                                                Intent i = new Intent(getApplicationContext(), StudentProfileActivity.class);
-                                                startActivity(i);
-                                            } else {
-                                                Toast.makeText(StudentEditPassword.this, "Network Error: Please try again after some time...", Toast.LENGTH_SHORT).show();
-
-                                            }
-                                        }
-                                    });
-
+                if (!pass1.equals("")) {
+                    if (!pass2.equals("")) {
+                        if (pass1.equals(pass2)) {
+                            updatePassword(studentId, pass1);
+                            Intent i = new Intent(getApplicationContext(), StudentProfileActivity.class);
+                            startActivity(i);
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Passwords should match!!", Toast.LENGTH_SHORT).show();
                         }
-
-                        @Override
-                        public void onCancelled(@NonNull DatabaseError error) {
-                            Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
-
-
-                        }
-
-                    });
-
+                    }
                 } else {
-                    Toast.makeText(getApplicationContext(), "Password was not matched!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "Enter both passwords", Toast.LENGTH_SHORT).show();
                 }
-
 
             }
         });
 
 
+    }
+
+    private void updatePassword(String studentId, String pass1) {
+        dbStudent = FirebaseDatabase.getInstance().getReference();
+        dbStudent.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                HashMap<String, Object> passwordDetails = new HashMap<>();
+                passwordDetails.put("password", pass1);
+
+                dbStudent.child(parentDbName).child(studentId).updateChildren(passwordDetails);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+
+
+            }
+
+        });
+    }
+
+    private void getCurrentPassword(String studentId) {
+        final DatabaseReference RootRef;
+        RootRef = FirebaseDatabase.getInstance().getReference();
+        RootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                if(snapshot.child(parentDbName).child(studentId).exists()){
+                    Users student =snapshot.child(parentDbName).child(studentId).getValue(Users.class);
+                    current_pass.setText(student.getPassword());
+
+
+                }else{
+                    Toast.makeText(getApplicationContext(),"Something went wrong",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getApplicationContext(),error.getMessage(),Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override

6f368c10e1a8b1153f9a5ed05f5a266520a8c226
Manjinder Singh <manjinders2212@gmail.com>
2020-11-02 11:47:10 -0500

    NotificationPojo work finish

--- app/src/main/java/com/example/cegepaas/Model/NotificationPojo.java
+++ app/src/main/java/com/example/cegepaas/Model/NotificationPojo.java
@@ -1,4 +1,52 @@
 package com.example.cegepaas.Model;
 
+import com.google.gson.annotations.SerializedName;
+
 public class NotificationPojo {
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+
+    @SerializedName("id")
+    private String id;
+
+    @SerializedName("msg")
+    private String msg;
+
+    @SerializedName("title")
+    private String title;
+
+    @SerializedName("uname")
+    private String uname;
+
+
 }
